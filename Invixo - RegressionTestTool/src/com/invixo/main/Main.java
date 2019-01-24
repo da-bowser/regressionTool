e6f454c8b4e5e7616b606a8755bc0dcea7f9f247
JSCH <JSCH@LAPTOP-5MIVD5TV.invixo.local>
2019-01-24 09:59:36 +0100

    More parameter handling

--- Invixo - RegressionTestTool/src/com/invixo/main/Main.java
+++ Invixo - RegressionTestTool/src/com/invixo/main/Main.java
@@ -1,9 +1,15 @@
 package com.invixo.main;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.invixo.common.GeneralException;
 import com.invixo.common.util.Logger;
+import com.invixo.common.util.Util;
 import com.invixo.compare.Comparer;
 import com.invixo.consistency.FileStructure;
 import com.invixo.extraction.IntegratedConfiguration;
@@ -54,7 +60,7 @@ public class Main {
 	public static String PARAM_VAL_HTTP_PORT 					= null;
 	
 	// SAP PO URL PREFIX/START. Example result: http://ipod.invixo.com:50000/
-	public static final String SAP_PO_HTTP_HOST_AND_PORT		= null;
+	public static String SAP_PO_HTTP_HOST_AND_PORT		= null;
 	
 	// Parameter: SAP XI sender adapter name
 	private static final String PARAM_KEY_XI_SENDER_ADAPTER		= "xiSenderAdapter";
@@ -66,35 +72,42 @@ public class Main {
 
 	
 	public static void main(String[] args) {
-		for (String param : args) {	
-			// Set input parameters			
-			if(param.contains(PARAM_KEY_ICO_REQUEST_FILES_ENV)) {
-				PARAM_VAL_ICO_REQUEST_FILES_ENV = param.replace(PARAM_KEY_ICO_REQUEST_FILES_ENV + "=", "");
-			} else if(param.contains(PARAM_KEY_BASE_DIR)) {
-				PARAM_VAL_BASE_DIR = param.replace(PARAM_KEY_BASE_DIR + "=", "");
-			} else if(param.contains(PARAM_KEY_TARGET_ENV)) {
-				PARAM_VAL_TARGET_ENV = param.replace(PARAM_KEY_TARGET_ENV + "=", "");
-			} else if(param.contains(PARAM_KEY_OPERATION)) {
-				PARAM_VAL_OPERATION = param.replace(PARAM_KEY_OPERATION + "=", "");
-			} else if(param.contains(PARAM_KEY_SOURCE_ENV)) {
-				PARAM_VAL_SOURCE_ENV = param.replace(PARAM_KEY_SOURCE_ENV + "=", "");
-			} else if(param.contains(PARAM_KEY_CREDENTIALS_FILE)) {
-				PARAM_VAL_CREDENTIALS_FILE = param.replace(PARAM_KEY_CREDENTIALS_FILE + "=", "");
-			} else if(param.contains(PARAM_KEY_HTTP_HOST)) {
-				PARAM_VAL_HTTP_HOST = param.replace(PARAM_KEY_HTTP_HOST + "=", "");
-			} else if(param.contains(PARAM_KEY_HTTP_PORT)) {
-				PARAM_VAL_HTTP_PORT = param.replace(PARAM_KEY_HTTP_PORT + "=", "");
-			} else if(param.contains(PARAM_KEY_XI_SENDER_ADAPTER)) {
-				PARAM_VAL_XI_SENDER_ADAPTER = param.replace(PARAM_KEY_XI_SENDER_ADAPTER + "=", "");
-			} else if(param.contains(PARAM_KEY_SENDER_COMPONENT)) {
-				PARAM_VAL_SENDER_COMPONENT = param.replace(PARAM_KEY_SENDER_COMPONENT + "=", "");
-			}
-		}
-		
 		try {
-			// Validate input parameters
-			validateParameters(PARAM_VAL_OPERATION);
+
+			for (String param : args) {	
+				// Set input parameters			
+				if(param.contains(PARAM_KEY_ICO_REQUEST_FILES_ENV)) {
+					PARAM_VAL_ICO_REQUEST_FILES_ENV = param.replace(PARAM_KEY_ICO_REQUEST_FILES_ENV + "=", "");
+				} else if(param.contains(PARAM_KEY_BASE_DIR)) {
+					PARAM_VAL_BASE_DIR = param.replace(PARAM_KEY_BASE_DIR + "=", "");
+				} else if(param.contains(PARAM_KEY_TARGET_ENV)) {
+					PARAM_VAL_TARGET_ENV = param.replace(PARAM_KEY_TARGET_ENV + "=", "");
+				} else if(param.contains(PARAM_KEY_OPERATION)) {
+					PARAM_VAL_OPERATION = param.replace(PARAM_KEY_OPERATION + "=", "");
+				} else if(param.contains(PARAM_KEY_SOURCE_ENV)) {
+					PARAM_VAL_SOURCE_ENV = param.replace(PARAM_KEY_SOURCE_ENV + "=", "");
+				} else if(param.contains(PARAM_KEY_CREDENTIALS_FILE)) {
+					PARAM_VAL_CREDENTIALS_FILE = param.replace(PARAM_KEY_CREDENTIALS_FILE + "=", "");
+				} else if(param.contains(PARAM_KEY_HTTP_HOST)) {
+					PARAM_VAL_HTTP_HOST = param.replace(PARAM_KEY_HTTP_HOST + "=", "");
+				} else if(param.contains(PARAM_KEY_HTTP_PORT)) {
+					PARAM_VAL_HTTP_PORT = param.replace(PARAM_KEY_HTTP_PORT + "=", "");
+				} else if(param.contains(PARAM_KEY_XI_SENDER_ADAPTER)) {
+					PARAM_VAL_XI_SENDER_ADAPTER = param.replace(PARAM_KEY_XI_SENDER_ADAPTER + "=", "");
+				} else if(param.contains(PARAM_KEY_SENDER_COMPONENT)) {
+					PARAM_VAL_SENDER_COMPONENT = param.replace(PARAM_KEY_SENDER_COMPONENT + "=", "");
+				}
+			}
 			
+			// Handle credential file and set program parameters
+			readAndSetCredentials(PARAM_VAL_CREDENTIALS_FILE);
+			
+			// Build complete PO host and port using imported parameters
+			buildCompletePOUrl(PARAM_VAL_HTTP_HOST, PARAM_VAL_HTTP_PORT);
+			
+			// Validate input parameters and sequentially build parameters
+			validateParameters(PARAM_VAL_OPERATION);
+
 			// Execute
 			if (Operation.extract.toString().equals(PARAM_VAL_OPERATION)) {
 				extract();
@@ -105,11 +118,40 @@ public class Main {
 			}
 		} catch (GeneralException e) {
 			// TODO: Not valid input, inform end user in the nicest way possible
+			System.out.println(e.toString());
 		}
 		
 	}
 
 	
+	private static void buildCompletePOUrl(String host, String port) {
+		SAP_PO_HTTP_HOST_AND_PORT = host + ":" + port + "/";
+	}
+
+
+	private static void readAndSetCredentials(String sourceDirectory) throws GeneralException {
+		
+		try {
+			// Get credential file
+			List<Path> credentialsFile = Util.generateListOfPaths(sourceDirectory, "FILE");
+			
+			// We expect only credentials file
+			List<String> credentialLines = Files.lines(credentialsFile.get(0)).collect(Collectors.toList());
+			
+			// Line 1: user name
+			CREDENTIAL_USER = credentialLines.get(0);
+			
+			// line 2: password
+			CREDENTIAL_PASS = credentialLines.get(1);
+			
+		} catch (IOException e) {
+			String msg = "Error | Problem reading credential fil from :" + sourceDirectory + e.getMessage();
+			throw new GeneralException(msg);
+		}
+		
+	}
+
+
 	private static void validateParameters(String operation) throws GeneralException {
 		// TODO: Validate
 	}
